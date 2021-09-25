f2cd4cccaeeab754f0c2e5bb918e87296be288a1
jhaboic <josiphaboic@gmail.com>
2021-09-25 23:20:00 +0200

    add example for composite functions

---
+++ examples/composite.rs
@@ -0,0 +1,33 @@
+#![feature(impl_trait_in_bindings)]
+#![allow(incomplete_features)]
+
+use mathplus::composite;
+
+
+pub fn composite_example_0() {
+	let linear_fn: impl Fn(f32) -> f32 = move |x: f32| {
+			2.0 * x + 4.0
+	};
+
+	let pow_2_fn: impl Fn(f32) -> f32 = move |x: f32| {
+			x * x
+	};
+
+	let u = composite::function(linear_fn, pow_2_fn);
+
+
+	for x in 0..=10 {
+		println!("x = {}; u(x) = {}", x as f32, u(x as f32));
+	}
+}
+
+pub fn main() {
+	println!("Composite Example 0");
+	println!("===================");
+	println!("f(x)    = 2.0 * x + 4.0");
+	println!("g(x)    = x * x");
+	println!("f(g(x)) = 2.0 (x * x) + 4.0");
+	println!("---------------------------");
+	composite_example_0();
+	println!();
+}
