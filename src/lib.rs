0b401ad0c5e2ecea26834bf35ce712b3596de69b
jhaboic <josiphaboic@gmail.com>
2021-09-26 15:34:10 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -12,98 +12,139 @@ pub mod composite {
 
 pub mod trigonometry {
 
-    pub fn vsin(theta: f32) -> f32 {
+    pub fn vsin(x: f32) -> f32 {
         //! versed sin
         //!
         //! 1 - cos(Θ)
-        1.0 - theta.cos()
+        1.0 - x.cos()
     }
     
-    pub fn cvsin(theta: f32) -> f32 {
+    pub fn cvsin(x: f32) -> f32 {
         //! coversed sin
         //!
         //! 1 - sin(Θ)
-        1.0 - theta.sin()
+        1.0 - x.sin()
     }
 
-    pub fn vcos(theta: f32) -> f32 {
+    pub fn vcos(x: f32) -> f32 {
         //! versed cos
         //!
         //! 1 + cos(Θ)
-        1.0 + theta.cos()
+        1.0 + x.cos()
     }
 
-    pub fn cvcos(theta: f32) -> f32 {
+    pub fn cvcos(x: f32) -> f32 {
         //! coversed cos
         //!
         //! 1 + sin(Θ)
-        1.0 + theta.sin()
+        1.0 + x.sin()
     }
     
-    pub fn hvsin(theta: f32) -> f32 {
+    pub fn hvsin(x: f32) -> f32 {
         //! half versed sin
         //!
         //! (1 - cos(Θ)) / 2
-        (1.0 - theta.cos()) / 2.0
+        (1.0 - x.cos()) / 2.0
     }
     
-    pub fn hcvsin(theta: f32) -> f32 {
+    pub fn hcvsin(x: f32) -> f32 {
         //! half coversed sin
         //!
         //! (1 - sin(Θ)) / 2
-        (1.0 - theta.sin()) / 2.0
+        (1.0 - x.sin()) / 2.0
     }
     
-    pub fn hvcos(theta: f32) -> f32 {
+    pub fn hvcos(x: f32) -> f32 {
         //! half versed cos
         //!
         //! (1 + cos(Θ)) / 2
-        (1.0 + theta.cos()) / 2.0
+        (1.0 + x.cos()) / 2.0
     }
     
-    pub fn hcvcos(theta: f32) -> f32 {
+    pub fn hcvcos(x: f32) -> f32 {
         //! half coversed cos
         //!
         //! (1 + sin(Θ)) / 2
-        (1.0 + theta.sin()) / 2.0
+        (1.0 + x.sin()) / 2.0
     }
 }
 
 pub mod calculus {
      pub mod derivate {
 
-         pub fn vsin(theta: f32) -> f32 {
-             theta.sin()
+         pub fn vsin(x: f32) -> f32 {
+             x.sin()
          }
 
-         pub fn vcos(theta: f32) -> f32 {
-             -theta.sin()
+         pub fn vcos(x: f32) -> f32 {
+             -x.sin()
          }
 
-         pub fn cvsin(theta: f32) -> f32 {
-             -theta.cos()
+         pub fn cvsin(x: f32) -> f32 {
+             -x.cos()
          }
 
-         pub fn cvcos(theta: f32) -> f32 {
-             theta.cos()
+         pub fn cvcos(x: f32) -> f32 {
+             x.cos()
          }
 
-         pub fn hvsin(theta: f32) -> f32 {
-             theta.sin() / 2.0
+         pub fn hvsin(x: f32) -> f32 {
+             x.sin() / 2.0
          }
 
-         pub fn hcvsin(theta: f32) -> f32 {
-             -theta.cos() / 2.0
+         pub fn hcvsin(x: f32) -> f32 {
+             -x.cos() / 2.0
          }
 
-         pub fn hvcos(theta: f32) -> f32 {
-             -theta.sin() / 2.0
+         pub fn hvcos(x: f32) -> f32 {
+             -x.sin() / 2.0
          }
 
-         pub fn hcvcos(theta: f32) -> f32 {
-             theta.cos() / 2.0
+         pub fn hcvcos(x: f32) -> f32 {
+             x.cos() / 2.0
          }
     }
 
-    pub mod integral {}
+    pub mod integral {
+
+        pub fn vsin(x: f32) -> f32 {
+            //! ∫ vsin(x) dx = x - sin x + C
+            x - x.sin()
+        }
+        
+        pub fn vcos(x: f32) -> f32 {
+            //! ∫ vcos(x) dx = x + sin x + C
+            x + x.sin()
+        }
+
+        pub fn cvsin(x: f32) -> f32 {
+            //! ∫ cvsin(x) dx = x + cos x + C
+            x + x.cos()
+        }
+
+        pub fn cvcos(x: f32) -> f32 {
+            //! ∫ cvcos(x) dx = x - cos x + C
+            x - x.cos()
+        }
+
+        pub fn hvsin(x: f32) -> f32 {
+            //! ∫ hvsin(x) dx = (x - sin x) / 2 + C
+            (x - x.sin()) / 2.0
+        }
+
+        pub fn hcvsin(x: f32) -> f32 {
+            //! ∫ hvsin(x) dx = (x + cos x) / 2 + C
+            (x + x.cos()) / 2.0
+        }
+
+        pub fn hvcos(x: f32) -> f32 {
+            //! ∫ hvsin(x) dx = (x + sin x) / 2 + C
+            (x + x.sin()) / 2.0
+        }
+        
+        pub fn hcvcos(x: f32) -> f32 {
+            //! ∫ hvsin(x) dx = (x - cos x) / 2 + C
+            (x - x.cos()) / 2.0
+        }
+    }
 }
